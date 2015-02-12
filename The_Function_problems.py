4017af95ad4bf2ceebc5bb00917b049c7a5888f9
wangshunping <lionelwang93@163.com>
2015-02-12 22:15:46 +0800

    add..

--- The_Function_problems.py
+++ The_Function_problems.py
@@ -64,9 +64,9 @@ def row(p, n):
     '''
     return [p+x for x in range(n)]
 
-comprehension_with_row = [row(x*20,20) for x in range(0,15)]
+comprehension_with_row = [row(x,20) for x in range(0,15)]
 
-comprehension_without_row = [[x*20+y for y in range(20)] for x in range(0,15)]
+comprehension_without_row = [[x+y for y in range(20)] for x in range(0,15)]
 
 
 
